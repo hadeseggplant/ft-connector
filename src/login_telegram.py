0bd6517376b864b09d4cf5229cf2acc2c5a2098a
hadeseggplant <>
2024-10-15 22:50:59 +0800

    chore(low): fmt

--- src/login_telegram.py
+++ src/login_telegram.py
@@ -2,12 +2,12 @@ from telethon import TelegramClient
 import configparser, os
 
 current_dir = os.path.dirname(__file__)
-config_file_path = os.path.join(current_dir, '..', 'config.ini')
+config_file_path = os.path.join(current_dir, "..", "config.ini")
 config = configparser.ConfigParser()
 config.read(config_file_path)
 
 TG_API_ID = int(config["DEFAULT"]["Telegram_Api_Id"])
-TG_API_HASH = config['DEFAULT']['Telegram_Api_Hash']
+TG_API_HASH = config["DEFAULT"]["Telegram_Api_Hash"]
 
 tg_client = TelegramClient("hti_session", TG_API_ID, TG_API_HASH)
 tg_client.start()
